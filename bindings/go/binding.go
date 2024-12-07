f1edb17ad7d4813b6adb0bbb18d19e505800428e
Akonwi Ngoh <akonwi@gmail.com>
2024-12-06 22:27:52 -0500

    wrap boilerplate for parser setup

--- bindings/go/binding.go
+++ bindings/go/binding.go
@@ -5,9 +5,30 @@ package tree_sitter_ard
 // // NOTE: if your language has an external scanner, add it here.
 import "C"
 
-import "unsafe"
+import (
+	"unsafe"
 
-// Get the tree-sitter Language for this grammar.
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+)
+
+// 0.0.1
 func Language() unsafe.Pointer {
 	return unsafe.Pointer(C.tree_sitter_ard())
+}
+
+func MakeParser() (*tree_sitter.Parser, error) {
+	parser := tree_sitter.NewParser()
+	err := parser.SetLanguage(tree_sitter.NewLanguage(Language()))
+	if err != nil {
+		return nil, err
+	}
+	return parser, nil
+}
+
+func Parse(source []byte) (*tree_sitter.Tree, error) {
+	parser, err := MakeParser()
+	if err != nil {
+		return nil, err
+	}
+	return parser.Parse(source, nil), nil
 }
